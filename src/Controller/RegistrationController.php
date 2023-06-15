3908e3f2b46118b0be7ec48f3b91f8aba769f371
Guillaume GAUVAIN <guillaume.gauvain.conseil@gmail.com>
2023-06-15 16:22:08 +0200

    Feat(Registration): update msg flash

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -76,8 +76,7 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_register');
         }
 
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Your email address has been verified. Welcome !');
 
         return $this->redirectToRoute('app_register');
     }
