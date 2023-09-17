9cfac9582dedad9a1086aeec2e3a1739876a96a7
Guillaume GAUVAIN <guillaume.gauvain.conseil@gmail.com>
2023-09-17 15:49:13 +0200

    chore(React): install react

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
